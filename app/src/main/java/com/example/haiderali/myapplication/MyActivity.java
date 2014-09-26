8e3b4d31243e6eb129843a0963db8b6ab9ad2017
mdhaidarali <haidar@amtechgcc.com>
2014-09-26 17:24:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/haiderali/myapplication/MyActivity.java
@@ -0,0 +1,18 @@
+package com.example.haiderali.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+
+
+}
